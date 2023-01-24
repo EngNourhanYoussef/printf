e7c7cdc167ee8d888999f38595bea3fcaee478c9
EngNourhanYoussef <nourhan201717@yahoo.com>
2023-01-24 01:54:53 -0800

    done

--- main.h
+++ main.h
@@ -96,7 +96,7 @@ int write_pointer(char buffer[], int ind, int length,
 			int width, int flags, char padd, char extra_c, int padd_start);
 
 int write_unsgnd(int is_negative, int ind,
-			char buffer[],int flags, int width, int precision, int size);
+			char buffer[] ,int flags, int width, int precision, int size);
 
 /****************** UTILS ******************/
 int is_printable(char);
@@ -107,4 +107,3 @@ long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif
-
